633b0de3e24a4cd96871c630d8ae5752bc720c86
Ella Hart <ella.scarlett.hart@gmail.com>
2023-04-21 10:08:08 +0200

    Js Variables and Arrays - Bootstrap5 Fundamentals - Gradients and KeyFrames

---
+++ backend.js
@@ -0,0 +1,17 @@
+var student1 = "Ella"//string
+var score = 100//integer
+var learningSpeed = 8.2//float
+var isLearning = true//boolean
+var currency = "$"//char
+
+console.log("Student name is:" + " " + student1);
+console.info("Student score is: " + score + "\n" + "And learning speed is:" + learningSpeed);
+
+var students = [
+    "Ella",
+    "John",
+    "Korina",
+    "Mary"
+]
+
+console.log(students[2]);
